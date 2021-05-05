33ef5596a5bd3d357c8212fde78a626fdb5bda27
kokhanskyi <79640742+kokhanskyi@users.noreply.github.com>
2021-05-05 17:34:31 +0300

    dz8

---
+++ index.js
@@ -0,0 +1,42 @@
+import galleryItems from "./gallery-items.js";
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  imageLightbox: document.querySelector(".lightbox__image"),
+  closeLightboxButton: document.querySelector(".lightbox__button"),
+};
+const listImages = galleryItems
+  .map(
+    (image) => `
+<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${image.original}"
+  >
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join("");
+refs.gallery.insertAdjacentHTML("beforeend", listImages);
+
+refs.gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") return;
+  refs.lightbox.classList.add("is-open");
+  refs.imageLightbox.src = event.target.dataset.source;
+  refs.imageLightbox.alt = event.target.alt;
+});
+
+refs.closeLightboxButton.addEventListener("click", (event) => {
+  refs.lightbox.classList.remove("is-open");
+  refs.imageLightbox.src = "";
+  refs.imageLightbox.alt = "";
+});
